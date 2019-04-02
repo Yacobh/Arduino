f7c802cbfbd5618d8949e11b844ff49eb8b009a0
Yacobh <jacobocordova@gmail.com>
2019-04-02 15:09:35 -0300

    Create fresa.ino

---
+++ Fresa/fresa.ino
@@ -0,0 +1,48 @@
+// y posiblemente encendido de motor 
+  verifica_paro();  
+}
+
+
+/*
+ * Funciones auxiliares principales
+ */
+//-----------------------------------------------------------
+  // Verifica que se haya pulsado algun boton de direccion
+  // verifica que sea posible ir en esa dirección 
+  // y activa el rele de esa dirección (engancha direccion)
+  // y arranca el motor() con rele de partida (variador)
+
+void verifica_direccion()
+{
+    
+}
+
+//-----------------------------------------------------------
+  // verifica que se haya pulsado el boton de marcha
+  // velocidad (rapido o lento)
+  // hace el enganche (engancha velocidad)
+void verifica_marcha()
+{
+  
+}
+
+// ----------------------------------------------------------
+  // Verifica que se haya pulsado el boton de paro
+  // desengancha reles direccion y velocidad 
+  // y posiblemente encendido de motor
+void verifica_paro()
+{
+  
+}
+
+/*
+ * Funciones auxiliares secundarias
+ */
+// ----------------------------------------------------------
+  // Verifica los botones
+  // verifica cual es el estado de todos los botones
+
+void verifica_botones()
+{
+  
+}
