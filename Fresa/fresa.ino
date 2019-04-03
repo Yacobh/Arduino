059a7f1a51280200885292c38bfaab5219d36b16
Yacobh <jacobocordova@gmail.com>
2019-04-03 17:21:22 -0300

    Update fresa.ino

--- Fresa/fresa.ino
+++ Fresa/fresa.ino
@@ -1,64 +1,55 @@
+/*
+ * Empresa: Electromining LTDA.
+ * 3 de abril 2019
+ * Autor Jacobo Cordova
+ */
 int a = 0;
 
 void setup() {
   Serial.begin(9600);
-  //Botones 24 V pull_down?
-  pinMode(I0_0, INPUT);
-  /* 
-  pinMode(I0_1, INPUT);
-  pinMode(I0_2, INPUT);
-  pinMode(I0_3, INPUT);
-  pinMode(I0_4, INPUT);
-  pinMode(I0_5, INPUT);
-  pinMode(I0_6, INPUT);
-  pinMode(I0_7, INPUT);
-  pinMode(I0_8, INPUT);
-  pinMode(I0_9, INPUT);
-  */
+  //Botones 24 V
+  // NORMALMENTE ESTAN EN CERO Y CUANDO SE PULSAN PASAN A 1
+  pinMode(I0_0, INPUT); // Arriba
+  pinMode(I0_1, INPUT); // Abajo
+  //pinMode(I0_2, INPUT); // Izquierda
+  //pinMode(I0_3, INPUT); // Derecha
+  //pinMode(I0_4, INPUT); // Adelante
+  //pinMode(I0_5, INPUT); // Atras
+  pinMode(I0_6, INPUT); // Parada
+  pinMode(I0_7, INPUT); // Lento
+  pinMode(I0_8, INPUT); // Rapido
+  //pinMode(I0_9, INPUT); // parte disponible
+
   // finales de carrera
-  pinMode(A0_0, INPUT);
-  /*
-  pinMode(A0_1, INPUT);
-  pinMode(A0_2, INPUT);
-  pinMode(A0_3, INPUT);
-  pinMode(A0_4, INPUT);
-  pinMode(A0_5, INPUT);
-  
-  */
+  pinMode(SCL, INPUT_PULLUP);
+  pinMode(SDA, INPUT_PULLUP);
+  pinMode(SCK, INPUT_PULLUP);
+
+  pinMode(MOSI, INPUT_PULLUP);
+  pinMode(MISO, INPUT_PULLUP);
   // Reles
-    // enganches
+  // enganches
   pinMode(Q0_0, OUTPUT);
-  /*
   pinMode(Q0_1, OUTPUT);
   pinMode(Q0_2, OUTPUT);
   pinMode(Q0_3, OUTPUT);
   pinMode(Q0_4, OUTPUT);
-    // motor
+  // motor
   pinMode(Q0_5, OUTPUT);
   pinMode(Q0_6, OUTPUT);
-   */
+
 }
 
 /*
- * Loop que realiza el control general de la fresadora.
- */
+   Loop que realiza el control general de la fresadora.
+*/
 void loop() {
-  /*
-  Serial.println(a);
-  a--;
-  delay(40);
-  
-  a = digitalRead ( I0_0 );
-  Serial.println(a);
-  digitalWrite(Q0_0, a);
-  delay(40);
-  */
   //-----------------------------------------------------------
   // Verifica que se haya pulsado algun boton de direccion
-  // verifica que sea posible ir en esa dirección 
+  // verifica que sea posible ir en esa dirección
   // y activa el rele de esa dirección (engancha direccion)
   // y arranca el motor() con rele de partida (variador)
-  verifica_direccion();   // Se pulsa rele direccion de motor       
+  verifica_direccion();   // Se pulsa rele direccion de motor
   //-----------------------------------------------------------
   // verifica que se haya pulsado el boton de marcha
   // velocidad (rapido o lento)
@@ -66,20 +57,20 @@ void loop() {
   verifica_marcha();
   // ----------------------------------------------------------
   // Verifica que se haya pulsado el boton de paro
-  // desengancha reles direccion y velocidad 
-  // y posiblemente encendido de motor 
-  verifica_paro();  
+  // desengancha reles direccion y velocidad
+  // y posiblemente encendido de motor
+  verifica_paro();
 }
 
 
 /*
- * Funciones auxiliares principales
- */
+   Funciones auxiliares principales
+*/
 //-----------------------------------------------------------
-  // Verifica que se haya pulsado algun boton de direccion
-  // verifica que sea posible ir en esa dirección 
-  // y activa el rele de esa dirección (engancha direccion)
-  // y arranca el motor() con rele de partida (variador)
+// Verifica que se haya pulsado algun boton de direccion
+// verifica que sea posible ir en esa dirección
+// y activa el rele de esa dirección (engancha direccion)
+// y arranca el motor() con rele de partida (variador)
 
 void verifica_direccion()
 {
@@ -89,57 +80,56 @@ void verifica_direccion()
 }
 
 //-----------------------------------------------------------
-  // verifica que se haya pulsado el boton de marcha
-  // velocidad (rapido o lento)
-  // hace el enganche (engancha velocidad)
+// verifica que se haya pulsado el boton de marcha
+// velocidad (rapido o lento)
+// hace el enganche (engancha velocidad)
 void verifica_marcha()
 {
-  
+
 }
 
 // ----------------------------------------------------------
-  // Verifica que se haya pulsado el boton de paro
-  // desengancha reles direccion y velocidad 
-  // y posiblemente encendido de motor
+// Verifica que se haya pulsado el boton de paro
+// desengancha reles direccion y velocidad
+// y posiblemente encendido de motor
 void verifica_paro()
 {
   Serial.println( verifica_fin_carrera() );
 }
 
 /*
- * Funciones auxiliares secundarias
- */
+   Funciones auxiliares secundarias
+*/
 // ----------------------------------------------------------
-  // Verifica los botones
-  // verifica cual es el estado de todos los botones
+// Verifica los botones
+// verifica cual es el estado de todos los botones
 
 int verifica_botones()
 {
 
-   if( digitalRead(I0_0) == 1) return 1;
-   return 0;
-  
+  if ( digitalRead(I0_0) == 1) return 1;
+  return 0;
+
 }
 
 // ----------------------------------------------------------
-  // Verifica los fines de carrera
-  // verifica cual es el estado de todos los fines de carrera
+// Verifica los fines de carrera
+// verifica cual es el estado de todos los fines de carrera
 
 int verifica_fin_carrera()
 {
   //if ( analogRead(A0_6) >= 200) return 6 ;
   //return 0;
 
-  return analogRead(A0_6);
-  
+  return digitalRead(SCL);
+
 }
 
 // ----------------------------------------------------------
-  // activa los reles
+// activa los reles
 
 void setea_reles()
 {
-//  lectura_arriba = analogRead(arriba);
-  
-}
+  //  lectura_arriba = analogRead(arriba);
 
+}
